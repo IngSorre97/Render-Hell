8e6eed7bb88c40a4df8eec11c425e4cc3bfb9c13
IngSorre97 <daniele.sorrenti1997@gmail.com>
2025-07-02 18:46:36 +0200

    Render Hell utilities

---
+++ Assets/RenderHell/Common/RenderPass/IBaseRenderPass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace IngSorre97.RenderHell
+{
+    interface IBaseRenderPass
+    {
+        event Action<ScriptableRenderContext, Camera> OnPreRender;
+        event Action<ScriptableRenderContext, Camera> OnPostRender;
+        bool ShouldExecutePass();
+    }
+}
