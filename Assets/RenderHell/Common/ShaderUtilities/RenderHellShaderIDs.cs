c30961e55551cc770886b4687d691c53a1d3653a
Sorre97 <daniele.sorrenti1997@gmail.com>
2025-07-03 22:13:58 +0200

    Moved ShaderIDs and aligned to IBrush3D interface

--- Assets/RenderHell/Common/ShaderUtilities/RenderHellShaderIDs.cs
+++ Assets/RenderHell/Common/ShaderUtilities/RenderHellShaderIDs.cs
@@ -6,10 +6,12 @@ namespace IngSorre97.RenderHell
     {
         public static int SelectionMask { get; } = Shader.PropertyToID("_SelectionMask");
         public static int SelectionMaskSize { get; } = Shader.PropertyToID("_SelectionMaskSize");
-        public static int CursorObjPos { get; } = Shader.PropertyToID("_CursorObjPos");
-        public static int CursorRadius { get; } = Shader.PropertyToID("_CursorRadius");
+        public static int CursorNormalizedPos { get; } = Shader.PropertyToID("_CursorNormalizedPos");
+        public static int CursorNormalizedRadius { get; } = Shader.PropertyToID("_CursorNormalizedRadius");
         public static int BoundsMin { get; } = Shader.PropertyToID("_BoundsMin");
         public static int BoundsMax { get; } = Shader.PropertyToID("_BoundsMax");
         public static int Intersecting { get; } = Shader.PropertyToID("_Intersecting");
+        public static int Drawing { get; } = Shader.PropertyToID("_Drawing");
+        public static int Clipping { get; } = Shader.PropertyToID("_Clipping");
     }
 }
